20940adf6de335aa56d9c919576fbd35d1a6d19d
Ethan Burns <burns.ethan@gmail.com>
2011-09-12 08:28:16 -0400

    Add a pddltest program.

---
+++ pddltest/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io/ioutil"
+	"pddl"
+)
+
+const dump = false
+
+func main() {
+	s, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic("Error reading standard input")
+	}
+
+	p := pddl.Parse(pddl.Lex("stdin", string(s)))
+	d := p.ParseDomain()
+//	d := p.ParseProblem()
+	if (dump) {
+		fmt.Printf("%+v\n", d)
+	}
+}

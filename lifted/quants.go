2f181b22c7c6db106eff196cbc37bef7e8162196
Ethan Burns <burns.ethan@gmail.com>
2011-09-24 21:38:11 -0400

    More work on expanding quantifiers.

--- lifted/quants.go
+++ lifted/quants.go
@@ -1,5 +1,11 @@
 package lifted
 
+import "github.com/willf/bitset"
+
+func (l *Literal) expandQuants(s *Symtab, f *expFrame) *Literal {
+	return nil
+}
+
 func (e ExprTrue) expandQuants(*Symtab, *expFrame) Expr {
 	return e
 }
@@ -37,15 +43,45 @@ func (e *ExprNot) expandQuants(s *Symtab, f *expFrame) Expr {
 }
 
 func (e *ExprForall) expandQuants(s *Symtab, f *expFrame) Expr {
-	return nil
+	seen := bitset.New(uint(len(s.constNames)))
+	vr := e.Variable.Name.Num
+	conj := Expr(ExprTrue(1))
+	for i, _ := range e.Variable.Type {
+		for obj := range s.typeObjs[e.Variable.Type[i].Num] {
+			if seen.Test(uint(obj)) {
+				continue
+			}
+			frame := f.push(vr, obj)
+			conj = ExprConj(conj, e.Expr.expandQuants(s, frame))
+			if _, ok := conj.(ExprFalse); ok {
+				return ExprFalse(0)
+			}
+		}
+	}
+	return conj
 }
 
 func (e *ExprExists) expandQuants(s *Symtab, f *expFrame) Expr {
-	return nil
+	seen := bitset.New(uint(len(s.constNames)))
+	vr := e.Variable.Name.Num
+	disj := Expr(ExprFalse(0))
+	for i, _ := range e.Variable.Type {
+		for obj := range s.typeObjs[e.Variable.Type[i].Num] {
+			if seen.Test(uint(obj)) {
+				continue
+			}
+			frame := f.push(vr, obj)
+			disj = ExprDisj(disj, e.Expr.expandQuants(s, frame))
+			if _, ok := disj.(ExprTrue); ok {
+				return ExprTrue(1)
+			}
+		}
+	}
+	return disj
 }
 
 func (e *ExprLiteral) expandQuants(s *Symtab, f *expFrame) Expr {
-	return nil
+	return (*ExprLiteral)((*Literal)(e).expandQuants(s, f))
 }
 
 type expFrame struct {
@@ -61,4 +97,8 @@ func (f *expFrame) lookup(vr int) (int, bool) {
 		return f.vl, true
 	}
 	return f.up.lookup(vr)
+}
+
+func (f *expFrame) push(vr int, vl int) *expFrame {
+	return &expFrame { vr: vr, vl: vl, up: f }
 }

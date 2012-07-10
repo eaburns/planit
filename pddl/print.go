1c93ff0df5213cafe31c5ae3860a458ef2209520
Ethan Burns <burns.ethan@gmail.com>
2012-07-10 17:43:40 -0400

    Cleanup the pretty printing code. Now it isn't totally embarrassing when someone else takes a look at it.

--- pddl/print.go
+++ pddl/print.go
@@ -5,120 +5,132 @@ import (
 	"io"
 )
 
-const indent = "\t"
-
 // PrintDomain prints the domain in valid PDDL to the given writer.
 func PrintDomain(w io.Writer, d *Domain) {
 	fmt.Fprintf(w, "(define (domain %s)\n", d.Identifier)
 	printRequirements(w, d.Requirements)
-
-	if len(d.Types) > 0 {
-		fmt.Fprintf(w, "%s(:types", indent)
-		var ids []TypedIdentifier
-		for _, t := range d.Types {
-			if t.Location.Line == 0 {
-				// skip undeclared implicit types like object
-				continue
-			}
-			ids = append(ids, t.TypedIdentifier)
-		}
-		printTypedNames(w, "\n"+indent+indent, ids)
-		fmt.Fprintln(w, ")")
-	}
-
-	if len(d.Constants) > 0 {
-		fmt.Fprintf(w, "%s(:constants", indent)
-		printTypedNames(w, "\n"+indent+indent, d.Constants)
-		fmt.Fprintln(w, ")")
-	}
-
-	if len(d.Predicates) > 0 {
-		fmt.Fprintf(w, "%s(:predicates\n", indent)
-		for i, p := range d.Predicates {
-			if p.Location.Line == 0 {
-				// skip undefined implicit predicates like =
-				continue
-			}
-			fmt.Fprintf(w, "%s(%s", indent+indent, p.Str)
-			printTypedNames(w, " ", p.Parameters)
-			fmt.Fprint(w, ")")
-			if i < len(d.Predicates)-1 {
-				fmt.Fprint(w, "\n")
-			}
-		}
-		fmt.Fprintln(w, ")")
-	}
-
-	if len(d.Functions) > 0 {
-		fmt.Fprintf(w, "%s(:functions\n", indent)
-		for i, f := range d.Functions {
-			fmt.Fprintf(w, "%s(%s", indent+indent, f.Str)
-			printTypedNames(w, " ", f.Parameters)
-			fmt.Fprint(w, ")")
-			if len(f.Types) > 0 {
-				fmt.Fprint(w, " - ", typeString(f.Types))
-			}
-			if i < len(d.Functions)-1 {
-				fmt.Fprint(w, "\n")
-			}
-		}
-		fmt.Fprintln(w, ")")
-	}
-
+	printTypesDef(w, d.Types)
+	printConstsDef(w, ":constants", d.Constants)
+	printPredsDef(w, d.Predicates)
+	printFuncsDef(w, d.Functions)
 	for _, act := range d.Actions {
-		fmt.Fprintf(w, "%s(:action %s\n", indent, act.Identifier)
-		fmt.Fprintf(w, "%s:parameters (", indent+indent+indent)
-		printTypedNames(w, "", act.Parameters)
-		fmt.Fprint(w, ")")
-		if act.Precondition != nil {
-			fmt.Fprint(w, "\n")
-			fmt.Fprintf(w, "%s:precondition\n", indent+indent+indent)
-			act.Precondition.print(w, indent+indent+indent+indent)
-		}
-		if act.Effect != nil {
-			fmt.Fprint(w, "\n")
-			fmt.Fprintf(w, "%s:effect\n", indent+indent+indent)
-			act.Effect.print(w, indent+indent+indent+indent)
-		}
-		fmt.Fprintln(w, ")")
+		printAction(w, act)
 	}
+	fmt.Fprintln(w, ")")
+}
 
+func printTypesDef(w io.Writer, ts []Type) {
+	if len(ts) == 0 {
+		return
+	}
+	fmt.Fprintf(w, "%s(:types", indent(1))
+	var ids []TypedIdentifier
+	for _, t := range ts {
+		if t.Location.Line == 0 {
+			// skip undeclared implicit types like object
+			continue
+		}
+		ids = append(ids, t.TypedIdentifier)
+	}
+	printTypedNames(w, "\n"+indent(2), ids)
+	fmt.Fprintln(w, ")")
+}
+
+func printConstsDef(w io.Writer, def string, cs []TypedIdentifier) {
+	if len(cs) == 0 {
+		return
+	}
+	fmt.Fprintf(w, "%s(%s", indent(1), def)
+	printTypedNames(w, "\n"+indent(2), cs)
+	fmt.Fprintln(w, ")")
+}
+
+func printPredsDef(w io.Writer, ps []Predicate) {
+	if len(ps) == 0 {
+		return
+	}
+	fmt.Fprintf(w, "%s(:predicates\n", indent(1))
+	for i, p := range ps {
+		if p.Location.Line == 0 {
+			// skip undefined implicit predicates like =
+			continue
+		}
+		fmt.Fprintf(w, "%s(%s", indent(2), p.Str)
+		printTypedNames(w, " ", p.Parameters)
+		fmt.Fprint(w, ")")
+		if i < len(ps)-1 {
+			fmt.Fprint(w, "\n")
+		}
+	}
+	fmt.Fprintln(w, ")")
+}
+
+func printFuncsDef(w io.Writer, fs []Function) {
+	if len(fs) == 0 {
+		return
+	}
+	fmt.Fprintf(w, "%s(:functions\n", indent(1))
+	for i, f := range fs {
+		fmt.Fprintf(w, "%s(%s", indent(2), f.Str)
+		printTypedNames(w, " ", f.Parameters)
+		fmt.Fprint(w, ")")
+		if len(f.Types) > 0 {
+			fmt.Fprint(w, " - ", typeString(f.Types))
+		}
+		if i < len(fs)-1 {
+			fmt.Fprint(w, "\n")
+		}
+	}
+	fmt.Fprintln(w, ")")
+}
+
+func printAction(w io.Writer, act Action) {
+	fmt.Fprintf(w, "%s(:action %s\n", indent(1), act.Identifier)
+	fmt.Fprintf(w, "%s:parameters (", indent(3))
+	printTypedNames(w, "", act.Parameters)
+	fmt.Fprint(w, ")")
+	if act.Precondition != nil {
+		fmt.Fprint(w, "\n")
+		fmt.Fprintf(w, "%s:precondition\n", indent(3))
+		act.Precondition.print(w, indent(4))
+	}
+	if act.Effect != nil {
+		fmt.Fprint(w, "\n")
+		fmt.Fprintf(w, "%s:effect\n", indent(3))
+		act.Effect.print(w, indent(4))
+	}
 	fmt.Fprintln(w, ")")
 }
 
 // PrintProblem prints the problem in valid PDDL to the given writer.
 func PrintProblem(w io.Writer, p *Problem) {
-	fmt.Fprintf(w, "(define (problem %s)\n%s(:domain %s)\n", p.Identifier, indent, p.Domain)
+	fmt.Fprintf(w, "(define (problem %s)\n%s(:domain %s)\n", p.Identifier, indent(1), p.Domain)
 	printRequirements(w, p.Requirements)
 
-	if len(p.Objects) > 0 {
-		fmt.Fprintf(w, "%s(:objects", indent)
-		printTypedNames(w, "\n"+indent+indent, p.Objects)
-		fmt.Fprintln(w, ")")
-	}
+	printConstsDef(w, ":objects", p.Objects)
 
-	fmt.Fprintf(w, "%s(:init", indent)
+	fmt.Fprintf(w, "%s(:init", indent(1))
 	for _, f := range p.Init {
 		fmt.Fprint(w, "\n")
-		f.print(w, indent+indent)
+		f.print(w, indent(2))
 	}
 	fmt.Fprint(w, ")\n")
 
-	fmt.Fprintf(w, "%s(:goal\n", indent)
-	p.Goal.print(w, indent+indent)
+	fmt.Fprintf(w, "%s(:goal\n", indent(1))
+	p.Goal.print(w, indent(2))
 
 	fmt.Fprintln(w, ")\n)")
 }
 
 func printRequirements(w io.Writer, reqs []Identifier) {
 	if len(reqs) > 0 {
-		fmt.Fprintf(w, "%s(:requirements\n", indent)
+		fmt.Fprintf(w, "%s(:requirements\n", indent(1))
 		for i, r := range reqs {
 			s := r.Str
 			if i == len(reqs)-1 {
 				s += ")"
 			}
-			fmt.Fprintln(w, indent+indent, s)
+			fmt.Fprintln(w, indent(2), s)
 		}
 	}
 }
@@ -210,7 +222,7 @@ func (n *AndNode) print(w io.Writer, prefix string) {
 	fmt.Fprintf(w, "%s(and", prefix)
 	for _, f := range n.Formula {
 		fmt.Fprint(w, "\n")
-		f.print(w, prefix+indent)
+		f.print(w, prefix+indent(1))
 	}
 	fmt.Fprint(w, ")")
 }
@@ -219,22 +231,22 @@ func (n *OrNode) print(w io.Writer, prefix string) {
 	fmt.Fprintf(w, "%s(or", prefix)
 	for _, f := range n.Formula {
 		fmt.Fprint(w, "\n")
-		f.print(w, prefix+indent)
+		f.print(w, prefix+indent(1))
 	}
 	fmt.Fprint(w, ")")
 }
 
 func (n *NotNode) print(w io.Writer, prefix string) {
 	fmt.Fprintf(w, "%s(not\n", prefix)
-	n.Formula.print(w, prefix+indent)
+	n.Formula.print(w, prefix+indent(1))
 	fmt.Fprint(w, ")")
 }
 
 func (n *ImplyNode) print(w io.Writer, prefix string) {
 	fmt.Fprintf(w, "%s(imply\n", prefix)
-	n.Left.print(w, prefix+indent)
+	n.Left.print(w, prefix+indent(1))
 	fmt.Fprint(w, "\n")
-	n.Right.print(w, prefix+indent)
+	n.Right.print(w, prefix+indent(1))
 	fmt.Fprint(w, ")")
 }
 
@@ -242,7 +254,7 @@ func (n *ForallNode) print(w io.Writer, prefix string) {
 	fmt.Fprintf(w, "%s(forall (", prefix)
 	printTypedNames(w, "", n.Variables)
 	fmt.Fprint(w, ")\n")
-	n.Formula.print(w, prefix+indent)
+	n.Formula.print(w, prefix+indent(1))
 	fmt.Fprint(w, ")")
 }
 
@@ -250,18 +262,25 @@ func (n *ExistsNode) print(w io.Writer, prefix string) {
 	fmt.Fprintf(w, "%s(exists (", prefix)
 	printTypedNames(w, "", n.Variables)
 	fmt.Fprint(w, ")\n")
-	n.Formula.print(w, prefix+indent)
+	n.Formula.print(w, prefix+indent(1))
 	fmt.Fprint(w, ")")
 }
 
 func (n *WhenNode) print(w io.Writer, prefix string) {
 	fmt.Fprintf(w, "%s(when\n", prefix)
-	n.Condition.print(w, prefix+indent)
+	n.Condition.print(w, prefix+indent(1))
 	fmt.Fprint(w, "\n")
-	n.Formula.print(w, prefix+indent)
+	n.Formula.print(w, prefix+indent(1))
 	fmt.Fprint(w, ")")
 }
 
 func (n *AssignNode) print(w io.Writer, prefix string) {
 	fmt.Fprintf(w, "%s(%s %s %s)", prefix, n.Op, n.Lval.Str, n.Rval)
+}
+
+func indent(n int) (s string) {
+	for i := 0; i < n; i++ {
+		s += "\t"
+	}
+	return
 }

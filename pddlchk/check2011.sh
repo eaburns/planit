dd2341bd0b5c4022cb7b9d55c9a254331ff1c03e
Ethan Burns <burns.ethan@gmail.com>
2012-07-18 14:44:18 -0400

    Add a check script for IPC 2011 domains.

---
+++ pddlchk/check2011.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+dirs=/home/aifs2/group/data/pddl/ipc2011/seq-sat
+
+for dir in $dirs/*; do
+	for prob in $dir/problems/*.pddl ; do
+		pnum=$(echo $(basename $prob) | sed 's/.pddl//g')
+
+		dom=$dir/domain/${pnum}-domain.pddl
+		if ! test -e $dom; then
+			dom=$(ls $dir/domain/*.pddl | head -n 1)
+		fi
+
+		echo -n "$(basename $dir) $(basename $dom) $(basename $prob)… "
+		time -f "%E %M kB" ./pddlchk $dom $prob || {
+			echo ./pddlchk $dom $prob
+			exit 1
+		}
+	done
+done
